53772bcabbfac750c56643b4a467e6a8bca89bcb
Yasmin Sarinho <yasmiinsarinho@gmail.com>
2025-06-26 23:41:37 -0300

    feat/tela de login

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,120 @@
+import { TextField, Button } from "@mui/material";
+import axios from "axios";
+import { useState } from "react"
+import { useNavigate } from 'react-router-dom';
+
+const Login = () => {
+    const navigate = useNavigate();
+
+    const [formData, setFormData] = useState({
+        login: '',
+        senha: ''
+    });
+
+    const handleChange = (e) => {
+        setFormData({
+          ...formData,
+          [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios.post('http://localhost:8080/usuarios/autenticacao', formData)
+        .then(() => navigate('/contato/visualizar'))
+        .catch(erro => console.log(erro));
+
+    };
+
+
+    return (
+        <div style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh',
+          width: '100vw',
+          backgroundColor: '#f5f5f5'
+        }}>
+          <div style={{
+            maxWidth: '420px',
+            margin: '40px auto',
+            padding: '30px',
+            backgroundColor: '#fff',
+            borderRadius: '20px',
+            boxShadow: '0 8px 24px rgba(0,0,0,0.1)',
+            fontFamily: 'roboto'
+          }}>
+            <h2 style={{
+              textAlign: 'center',
+              fontSize: '24px',
+              fontWeight: '600',
+              color: '#000',
+              marginBottom: '20px'
+            }}>
+              Login
+            </h2>
+            
+            <form onSubmit={handleSubmit }>
+                <TextField
+                    id="login"
+                    label="Login"
+                    name="login"
+                    value={formData.login}
+                    onChange={handleChange}
+                    variant="outlined"
+                    fullWidth
+                    required
+                    sx={{
+                        marginBottom: '16px',
+                        '& .MuiOutlinedInput-root': {
+                        borderRadius: '12px'
+                        }
+                    }}  
+                />
+                <TextField
+                    id="outlined-password-input"
+                    label="Senha"
+                    name="senha"
+                    type="password"
+                    autoComplete="current-password"
+                    value={formData.senha}
+                    onChange={handleChange}
+                    variant="outlined"
+                    fullWidth
+                    required
+                    sx={{
+                        marginBottom: '16px',
+                        '& .MuiOutlinedInput-root': {
+                        borderRadius: '12px'
+                        }
+                    }}  
+                />
+
+                <Button
+                    variant="contained"
+                    color="primary"
+                    type="submit"
+                    fullWidth
+
+                    sx={{
+                    backgroundColor: '#007AFF',
+                    borderRadius: '12px',
+                    textTransform: 'none',
+                    fontWeight: '600',
+                    fontSize: '16px',
+                    padding: '10px 0',
+                    '&:hover': {
+                        backgroundColor: '#005FCC'
+                    }
+                    }}
+                >
+                    Logar
+                </Button>
+            </form>
+          </div>
+        </div>
+    );
+}
+
+export default Login;
