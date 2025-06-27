ca3943dce6d15afd31bdb3002bb879fa9131c41e
Henrique-Arcilio <aldomario.arcilio@outlook.com>
2025-06-26 22:24:37 -0300

    feat: adiciona encriptatação de senhas

--- backend/src/main/java/com/example/agenda/model/services/UsuarioService.java
+++ backend/src/main/java/com/example/agenda/model/services/UsuarioService.java
@@ -4,6 +4,7 @@ import com.example.agenda.model.entities.Usuario;
 import com.example.agenda.model.controller.dto.UsuarioLogarDto;
 import com.example.agenda.model.repository.UsuarioRepository;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,16 +12,18 @@ import java.util.Optional;
 @Service
 public class UsuarioService {
     private UsuarioRepository repository;
+    private PasswordEncoder encriptador;
 
-    public UsuarioService(UsuarioRepository repository) {
+    public UsuarioService(UsuarioRepository repository, PasswordEncoder encriptador) {
         this.repository = repository;
+        this.encriptador = encriptador;
     }
 
     public HttpSession autenticar(UsuarioLogarDto usuarioLogarDto, HttpSession session){
         String login = usuarioLogarDto.getLogin();
         Usuario usuario = repository.findUsuariosByLogin(login).orElseGet(null);
         if(usuario != null){
-            if(usuario.getSenha().equals(usuarioLogarDto.getSenha())){
+            if(encriptador.matches(usuario.getSenha(), usuario.getSenha())){
                  session.setAttribute("usuario", usuario);
                  return session;
             }
@@ -32,6 +35,8 @@ public class UsuarioService {
         if(optionalUsuario.isPresent()){
             return false;
         }
+        String senhaEncriptada = encriptador.encode(usuario.getSenha());
+        usuario.setSenha(senhaEncriptada);
         repository.save(usuario);
         return true;
     }
