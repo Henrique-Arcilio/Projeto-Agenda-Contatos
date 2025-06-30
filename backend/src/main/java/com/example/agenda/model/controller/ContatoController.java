0c97139656e9d99da5905bd274a6050b80f9970e
LucasCruzOliveira <cruz.lk272@gmail.com>
2025-06-30 17:34:05 -0300

    feat/Adicionado a logica de realizar busca via nome ou email

--- backend/src/main/java/com/example/agenda/model/controller/ContatoController.java
+++ backend/src/main/java/com/example/agenda/model/controller/ContatoController.java
@@ -2,16 +2,20 @@ package com.example.agenda.model.controller;
 
 import com.example.agenda.model.dto.ContatoEditarDTO;
 import com.example.agenda.model.entities.Contato;
+import com.example.agenda.model.repository.ContatoRepository;
 import com.example.agenda.model.services.ContatoService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/contatos")
 public class ContatoController {
+
+    private ContatoRepository contatoRepository;
     private ContatoService contatoService;
 
     public ContatoController(ContatoService contatoService) {
@@ -33,5 +37,15 @@ public class ContatoController {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @GetMapping("/buscar-por-nome")
+    public List<Contato> buscarPorNome(@RequestParam String nome) {
+        return contatoRepository.buscarPorNome(nome);
+    }
+
+    @GetMapping("/buscar-por-email")
+    public List<Contato> buscarPorEmail(@RequestParam String email) {
+        return contatoRepository.buscarPorEmail(email);
     }
 }
